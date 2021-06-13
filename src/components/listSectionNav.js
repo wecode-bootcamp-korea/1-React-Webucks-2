607d02e0d38df1608717f133c7e5367500ef3193
undefinedP <kzbaram@gmail.com>
2021-06-14 00:37:20 +0900

    MODIFY: this.props.name

--- src/components/listSectionNav.js
+++ src/components/listSectionNav.js
@@ -5,7 +5,7 @@ const SectionBar = () => {
   return (
     <div>
       <GiCoffeeCup />
-      <span className="coldbrew">콜드 브루 커피</span>
+      <span className="sectionName">{this.props.name}</span>
       <span className="info">
         디카페인 에스프레소 샷 추가 가능 (일부 음료 제외)
       </span>
