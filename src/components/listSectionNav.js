8e58ac585f78d81e24b58d38178b1441af375794
undefinedP <kzbaram@gmail.com>
2021-06-14 03:10:49 +0900

    MODIFY: this undefined 해결

--- src/components/listSectionNav.js
+++ src/components/listSectionNav.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import { GiCoffeeCup } from "react-icons/gi";
 
-const SectionBar = () => {
-  return (
-    <div>
-      <GiCoffeeCup />
-      <span className="sectionName">{this.props.name}</span>
-      <span className="info">
-        디카페인 에스프레소 샷 추가 가능 (일부 음료 제외)
-      </span>
-    </div>
-  );
-};
+class SectionNav extends Component {
+  render() {
+    return (
+      <div>
+        <GiCoffeeCup />
+        <span className="sectionName">{this.props.name}</span>
+        <span className="info">
+          디카페인 에스프레소 샷 추가 가능 (일부 음료 제외)
+        </span>
+      </div>
+    );
+  }
+}
 
-export default SectionBar;
+export default SectionNav;
