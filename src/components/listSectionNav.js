6681ca24a3f8d7d5b5ea9ef9691cf74a11e9bf1a
undefinedP <kzbaram@gmail.com>
2021-06-14 03:12:04 +0900

    MODIFY: Replace Icon

--- src/components/listSectionNav.js
+++ src/components/listSectionNav.js
@@ -5,8 +5,8 @@ class SectionNav extends Component {
   render() {
     return (
       <div>
-        <GiCoffeeCup />
         <span className="sectionName">{this.props.name}</span>
+        <GiCoffeeCup />
         <span className="info">
           디카페인 에스프레소 샷 추가 가능 (일부 음료 제외)
         </span>
