fb5a12019580a6dcbce4b043f2c6c75a270d331c
undefinedP <kzbaram@gmail.com>
2021-06-16 23:18:40 +0900

    ADD : Missioin 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/MJ/Login/LoginMJ.js
+++ src/pages/MJ/Login/LoginMJ.js
@@ -2,6 +2,24 @@ import React, { Component } from "react";
 import "./LoginMJ.scss";
 
 class LoginMJ extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: "",
+      password: "",
+    };
+  }
+
+  handleIdInput = event => {
+    console.log("id?", event.target.value);
+    this.setState({ id: event.target.value });
+  };
+
+  handlePasswordInput = event => {
+    console.log("pw?", event.target.value);
+    this.setState({ password: event.target.value });
+  };
+
   render() {
     return (
       <main className="main-container">
@@ -9,12 +27,14 @@ class LoginMJ extends Component {
           <header className="logo">Webucks</header>
           <form className="login">
             <input
+              onChange={this.handleIdInput}
               className="loginID"
               name="id"
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
             />
             <input
+              onChange={this.handlePasswordInput}
               className="loginPW"
               name="password"
               type="password"
