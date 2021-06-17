820d33eb64f0de82bd1f4958eb31dc4cd8e2e336
hxyxneee <wlgus3096@gmail.com>
2021-06-17 13:57:37 +0900

    REFACTOR: 로그인 페이지 state 리팩토링

--- src/pages/JH/Login/LoginJH.js
+++ src/pages/JH/Login/LoginJH.js
@@ -7,35 +7,13 @@ class LoginJH extends Component {
     this.state = {
       inputIDValue: "",
       inputPWValue: "",
-      inputIDBorder: "bucksLogin",
-      inputPWBorder: "bucksLogin",
-      isButtonActive: false,
     };
   }
 
-  handleIDInput = event => {
-    this.setState({ inputIDValue: event.target.value }, () => {
-      this.state.inputIDValue.includes("@")
-        ? this.setState({ inputIDBorder: "bucksLogin active" })
-        : this.setState({ inputIDBorder: "bucksLogin" });
-    });
-  };
-
-  handlePWInput = event => {
-    const MIN_PASSWORD_LENGTH = 8;
-    this.setState({ inputPWValue: event.target.value }, () => {
-      this.state.inputPWValue.length >= MIN_PASSWORD_LENGTH
-        ? this.setState({ inputPWBorder: "bucksLogin active" })
-        : this.setState({ inputPWBorder: "bucksLogin" });
-    });
-  };
-
-  handleButtonColor = () => {
-    const MIN_PASSWORD_LENGTH = 8;
-    this.state.inputIDValue.includes("@") &&
-    this.state.inputPWValue.length >= MIN_PASSWORD_LENGTH
-      ? this.setState({ isButtonActive: true })
-      : this.setState({ isButtonActive: false });
+  handleInput = event => {
+    console.log(event.target.value);
+    console.log(event.target.name);
+    this.setState({ [event.target.name]: event.target.value });
   };
 
   switchPage = () => {
@@ -43,34 +21,45 @@ class LoginJH extends Component {
   };
 
   render() {
+    const { inputIDValue, inputPWValue } = this.state;
     return (
       <div className="Login">
         <div className="allContainer">
           <div className="bucksContainer">
             <h1 className="title">ZuckerBucks</h1>
             <div className="inputContainer">
-              <form action="main.html" id="loginForm">
+              <form action="/ListJH" id="loginForm">
                 <input
                   type="text"
                   id="inputIDValue"
-                  className={this.state.inputIDBorder}
+                  className={
+                    inputIDValue.includes("@")
+                      ? "bucksLogin active"
+                      : "bucksLogin"
+                  }
+                  name="inputIDValue"
                   placeholder="전화번호, 사용자 이름 또는 이메일"
                   required
-                  onKeyUp={this.handleButtonColor}
-                  onChange={this.handleIDInput}
+                  onChange={this.handleInput}
                 />
                 <input
                   type="password"
                   id="inputPWValue"
-                  className={this.state.inputPWBorder}
+                  className={
+                    inputPWValue.length >= 8
+                      ? "bucksLogin active"
+                      : "bucksLogin"
+                  }
+                  name="inputPWValue"
                   placeholder="비밀번호"
                   required
-                  onKeyUp={this.handleButtonColor}
-                  onChange={this.handlePWInput}
+                  onChange={this.handleInput}
                 />
                 <button
                   className={
-                    this.state.isButtonActive ? "bucksBtn active" : "bucksBtn"
+                    inputIDValue.includes("@") && inputPWValue.length >= 8
+                      ? "bucksBtn active"
+                      : "bucksBtn"
                   }
                   onClick={this.switchPage}
                 >
@@ -79,7 +68,7 @@ class LoginJH extends Component {
               </form>
             </div>
             <div className="forgetPassword">
-              <a href="main.html" className="bucksLink">
+              <a href="/" className="bucksLink">
                 비밀번호를 잊으셨나요?
               </a>
             </div>
