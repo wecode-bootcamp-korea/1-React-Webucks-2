1cddcb8337b270901d49a73074f37cd87c3134a3
hxyxneee <wlgus3096@gmail.com>
2021-06-17 14:17:37 +0900

    REFACTOR: List-beverage-component.js의 props 구조분해할당 사용

--- src/pages/JH/List/List-beverage-component.js
+++ src/pages/JH/List/List-beverage-component.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
-import { BiHeart } from "react-icons/bi";
+import { FiHeart } from "react-icons/fi";
 
 class Beverage extends Component {
   render() {
+    const { name, img } = this.props;
     return (
       <div className="grid">
-        <a href="main.html" className="coffeeImg">
-          <img src={this.props.img} alt="beverageImg" className="listImage" />
+        <a href="/" className="coffeeImg">
+          <img src={img} alt="beverageImg" className="listImage" />
         </a>
         <p>
-          {this.props.name} <BiHeart />
+          {name} <FiHeart />
         </p>
       </div>
     );
