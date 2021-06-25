d9a3063a2ee2ca2920b220e3f54b96a1c98ae79b
굼뜸이 <muhoya@sinsuhoui-MacBookPro.local>
2021-06-25 10:21:59 +0900

    mission2 완료

--- src/pages/SH/Login/LoginSH.js
+++ src/pages/SH/Login/LoginSH.js
@@ -8,6 +8,7 @@ class LoginSH extends Component {
     this.state = {
       idValue: "",
       pwValue: "",
+      btnColorState: false,
     };
   }
 
@@ -21,6 +22,12 @@ class LoginSH extends Component {
     this.setState({ pwValue: event.target.value });
   };
 
+  btnChangeColor = () => {
+    this.state.idValue.includes("@") && this.state.pwValue.length >= 5
+      ? this.setState({ btnColorState: true })
+      : this.setState({ btnColorState: false });
+  };
+
   render() {
     return (
       <>
@@ -29,25 +36,29 @@ class LoginSH extends Component {
             <header>
               <p>SooBucks</p>
             </header>
-            <nav className="loginInput1">
-              <input
-                onChange={this.handleIdInput}
-                type="Text"
-                className="inputId"
-                placeholder=" 전화번호, 사용자 이름 또는 이메일"
-              ></input>
-            </nav>
-            <nav className="loginInput2">
-              <input
-                onChange={this.handlePwInput}
-                type="password"
-                className="inputPw"
-                placeholder=" 비밀번호"
-              ></input>
-            </nav>
-            <div className="Login">
-              <button>로그인</button>
-            </div>
+            <form onKeyUp={this.btnChangeColor}>
+              <nav className="loginInput1">
+                <input
+                  onChange={this.handleIdInput}
+                  type="Text"
+                  className="inputId"
+                  placeholder=" 전화번호, 사용자 이름 또는 이메일"
+                ></input>
+              </nav>
+              <nav className="loginInput2">
+                <input
+                  onChange={this.handlePwInput}
+                  type="password"
+                  className="inputPw"
+                  placeholder=" 비밀번호"
+                ></input>
+              </nav>
+              <div className="Login">
+                <button className={this.state.btnColorState ? "active" : ""}>
+                  로그인
+                </button>
+              </div>
+            </form>
             <footer>
               <a>비밀번호를 잊으셨나요?</a>
             </footer>
